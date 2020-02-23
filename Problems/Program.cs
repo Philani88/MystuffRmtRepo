46551f02d6e25f93224788850f3186f8d690c97e
Philani Ngwenya <G17N3108@ict.ru.ac.za>
2020-02-23 19:46:14 +0200

    changes made to Problems.Test

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        string Prefix(string input )
+        {
+          if(input==null)
+         {
+            return null;
+         } 
+          string[] inputary= input.Split();
+          int numwords=inputary.Length;
+          int numchars=input.Length;
+          return ($"{numchars},{numwords}:input");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
